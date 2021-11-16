4a4d2ac274c9f2c58a714da120e6fb3651240572
Luke Hoang <nampanama0101@gmail.com>
2021-11-16 16:44:14 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const express = require('express');
 const routes = require('./src/routes/index.route');
 require("dotenv").config();
-
 const mongoose = require('mongoose');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 async function Connection(){
     try {
@@ -24,14 +24,12 @@ Connection();
 // use module
 const app = express();
 // global variables
+app.use('/api', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }));
 const PORT = process.env.PORT || 4000;
 app.use(cors());
-
 app.use(express.json());
 
 routes(app);
-app.get('/', (req, res) => {res.send('Helloworld')});
-
 // body
 app.listen(PORT, ()=>console.log(`App is listening at port ${PORT}`));
 
